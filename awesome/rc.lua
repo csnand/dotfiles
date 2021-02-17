0d3f0ba0a3de71e5dac24f94e87e173eb933f6d6
csnand <askmryang@outlook.com>
2021-02-17 07:16:48 +0000

    update config

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -574,6 +574,7 @@ client.connect_signal("unfocus", function(c) c.border_color = beautiful.border_n
 -- awful.spawn.with_shell()
 awful.spawn.with_shell("picom -b")
 awful.spawn.with_shell("nitrogen --restore")
+awful.spawn.with_shell("nm-applet")
 
 -- Window gaps
 beautiful.useless_gap = 5
